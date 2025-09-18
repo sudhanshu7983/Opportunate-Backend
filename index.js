45b0894c379fad3283d35653db3cd30b12aeb229
sudhanshu pathak <122100016+sudhanshu7983@users.noreply.github.com>
2025-09-19 00:38:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors";
+import cors from "cors"; // Using ES Modules import syntax
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
@@ -8,7 +8,7 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
@@ -18,8 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // ✅ CORS config for localhost and deployed frontend
-const cors = require('cors');
-
 app.use(
   cors({
     origin: [
@@ -30,7 +28,6 @@ app.use(
     credentials: true, // Allow cookies and credentials to be sent
   })
 );
-
 
 // Port
 const PORT = process.env.PORT || 3000;
